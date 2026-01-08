3abbcec700bade5e765cbfcd100d55edebacb132
leonardorosasilva <03466316022@edu.udesc.br>
2026-01-08 10:21:06 -0300

    backend com spring

---
+++ goalfyBackend/src/main/java/com/app/goalfybackend/GoalfyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.app.goalfybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoalfyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoalfyBackendApplication.class, args);
+    }
+
+}
---
+++ goalfyBackend/src/test/java/com/app/goalfybackend/GoalfyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.goalfybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoalfyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
