7c9ad730852afe70bf922c7b8eb973b4c80616bd
leonardorosasilva <03466316022@edu.udesc.br>
2026-01-10 16:52:22 -0300

    Descomentados os campos `address` e `city` na entidade `ClientsEntity` no backend

--- goalfyBackend/src/main/java/com/app/goalfybackend/infrastructure/entity/ClientsEntity.java
+++ goalfyBackend/src/main/java/com/app/goalfybackend/infrastructure/entity/ClientsEntity.java
@@ -36,11 +36,10 @@ public class ClientsEntity {
 
     @Column(name = "cep", nullable = false)
     private String cep;
-    /*
-     *    @Column(name = "address", nullable = false)
-     *    private String address;
-     *    
-     *    @Column(name = "city", nullable = false)
-     *    private String city;
-    */
+
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    @Column(name = "city", nullable = false)
+    private String city;
 }
