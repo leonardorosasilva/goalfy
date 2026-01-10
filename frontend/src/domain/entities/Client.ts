163202a98ce6c02f9f577e4bf0b5adc1f6ca95f0
leonardorosasilva <03466316022@edu.udesc.br>
2026-01-10 16:53:37 -0300

    Adicionada entidade `Client` e `ClientFormData` no frontend

---
+++ frontend/src/domain/entities/Client.ts
@@ -0,0 +1,21 @@
+export interface Client {
+  id: number;
+  name: string;
+  email: string;
+  telephone: string;
+  cnpj: string;
+  cep: string;
+  address: string;
+  city: string;
+}
+
+export interface ClientFormData {
+  name: string;
+  email: string;
+  telephone: string;
+  cnpj: string;
+  cep: string;
+  address: string;
+  city: string;
+}
+
