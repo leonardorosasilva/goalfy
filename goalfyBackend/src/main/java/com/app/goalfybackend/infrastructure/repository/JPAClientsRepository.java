88c697cc6a62d547ab130bcb778b4922e22056b5
leonardorosasilva <03466316022@edu.udesc.br>
2026-01-10 16:52:40 -0300

    Adicionados métodos de busca por `id`, `name`, `email` e `cnpj` ao repositório JPA `JPAClientsRepository` no backend

--- goalfyBackend/src/main/java/com/app/goalfybackend/infrastructure/repository/JPAClientsRepository.java
+++ goalfyBackend/src/main/java/com/app/goalfybackend/infrastructure/repository/JPAClientsRepository.java
@@ -2,8 +2,15 @@ package com.app.goalfybackend.infrastructure.repository;
 
 import com.app.goalfybackend.infrastructure.entity.ClientsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+
+@Repository
 public interface JPAClientsRepository extends JpaRepository<ClientsEntity, Long> {
-    
-
+    Optional<ClientsEntity> findById(Long id);
+    Optional<ClientsEntity> findByName(String name);
+    Optional<ClientsEntity> findByEmail(String email);
+    Optional<ClientsEntity> findByCnpj(String cnpj);
 }   
