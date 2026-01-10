bbe4dd8c3e31b0ece50ff6b2a1d4ca4ff8a2bb21
leonardorosasilva <03466316022@edu.udesc.br>
2026-01-10 16:52:50 -0300

    Removida importação desnecessária em `InvalidCnpjException` no backend

--- goalfyBackend/src/main/java/com/app/goalfybackend/domain/exception/InvalidCnpjException.java
+++ goalfyBackend/src/main/java/com/app/goalfybackend/domain/exception/InvalidCnpjException.java
@@ -1,5 +1,4 @@
 package com.app.goalfybackend.domain.exception;
-import com.app.goalfybackend.domain.exception.DomainException; 
 
 public class InvalidCnpjException extends DomainException {
     public InvalidCnpjException(String message) {
